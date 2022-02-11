c46c55a8e3fca0b300a0dd13718a23e2d5ca8ed4
NadyaYar <91716602+NadyaYar@users.noreply.github.com>
2022-02-11 15:53:51 +0200

    Update sortArrays.java

--- src/sortArrays.java
+++ src/sortArrays.java
@@ -1,17 +1,16 @@
 import java.util.Arrays;
 
-public class sortArrays {
+public class SortArrays {
     public static void main(String[] args) {
         int[] array1 = {1, 4, 3, 7, 2};
         System.out.println(Arrays.toString(arrayFromMinToMax(array1)));
         System.out.println(Arrays.toString(arrayFromMaxToMin(array1)));
     }
     static int[] arrayFromMinToMax(int[] array) {
-        int temp = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] > array[j]) {
-                    temp = array[i];
+                   int temp = array[i];
                     array[i] = array[j];
                     array[j] = temp;
                 }
@@ -20,11 +19,10 @@ public class sortArrays {
         return array;
     }
     static int[] arrayFromMaxToMin(int[] array) {
-        int temp = 0;
-        for (int i = 0; i < array.length; i++) {
+         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] < array[j]) {
-                    temp = array[i];
+                   int temp = array[i];
                     array[i] = array[j];
                     array[j] = temp;
                 }
